29ba4fb19909fac4a52ec2e2ca100c8dacddcab7
Nicole Abib <nicole.abib@duke.edu>
2019-02-04 19:10:55 -0500

    Addition of Gaby and Sophie's updates to flood routing

--- code/Flood_Routing_Main.m
+++ code/Flood_Routing_Main.m
@@ -10,12 +10,16 @@
 %1 Cubic Foot = 0.0283168466 m3
 %g = 9.81 m/s/s - Gravitational acceleration
 %--------------------------------------------------------------------------
-%clear all 
-%clc
+clear all 
+clc
 g=9.81;              % Gravitational constant (m/s/s)
 % ----------- Use geometry of Lake Wheeler dam ----------------------------
 % (https://en.wikipedia.org/wiki/Wheeler_Dam)
 Reservoir_Watershed_Parameters;
+%----------------------- Watershed area 
+Area_WS=50;                    % Watershed contributing to inflow (km2)
+%ftp://ftp.odot.state.or.us/techserv/Geo-Environmental/Hydraulics/Hydraulics%20Manual/Chapter_07/Chapter_07_appendix_F/CHAPTER_07_appendix_F.pdf
+C_runoff=0.5;                  % Runoff coefficient, varies with LULC
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 Nday=365*200;                   % Number of days to model (d)(*200)
 dt=0.01;                        % Time increment to compute outflow (d)
@@ -32,22 +36,21 @@ MM=length(UH);
 Dur=4/24;                       % Duration of storm (hours per day) %climate change model can feed in here
 annual_precip=1200;             % Annual precip. (mm/year)
 
-%%
-
 %--- Loop over return period of daily rainfall
 for kk=1:2 %why 29?
 Ret_P(kk)=kk+1; %stays
-freq=1/(Ret_P(kk));             % Return frequency between days (1/d %stays
-dep=(annual_precip/365)/freq;   % Expected water depth (mm) %stays ----ask gaby
+freq=1/(Ret_P(kk));             % Return frequency between days (1/d)
+
+dep=(annual_precip/365)/freq;   % Expected water depth (mm)
 P=[];
 [P] = Precip_generate_series(freq,dep,Dur,Nday,dt);
-Ntot=length(P); %stays
+Ntot=length(P);
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 %---- Assume PET
 PET = 0;                        % ET losses from reservoir (mm/d) factor in ET at some point
 %---- Convert rainfall to inflow using the unit hydrograph and runoff coef.
-Factor=C_runoff*(Area_WS*(1000*1000))*((1/Dur)*(0.001)); %stays
-Ids_F=Factor*conv(P,UH);    % Determine inflow from convolution of P and UH %stays
+Factor=C_runoff*(Area_WS*(1000*1000))*((1/Dur)*(0.001));
+Ids_F=Factor*conv(P,UH);    % Determine inflow from convolution of P and UH
 Ih1=[];
 Ih1=Ids_F(1:Ntot+1);        % This is the inflow in m3/d
 t=[0:1:Ntot]*dt;
@@ -64,7 +67,8 @@ for i=1:Ntot
   Od(i)=alpha*((Sd(i)+eps))^(beta); %storage outflow relationship
 %--- This is the mass balance equation:dS/dt = I - (S/alpha)^(1/beta)-ET 
   Sd(i+1)=max(Sd(i)+dt*(Ih1(i)-Od(i)-ET_RES),100*eps);
-  %Od - update with nutrient regressions script
+  
+  % -------Od - update with nutrient regressions script ------
   %--- Compute sediment amount given outflow
   %Sed(i) = slope1*Od(i) + intercept1;
   %--- Compute TN given outflow
@@ -73,7 +77,6 @@ for i=1:Ntot
   %Ph(i) = slope3*Od(i) + intercept3;
   %--- Compute DO given outflow
   %dO(i) = slope2*Od(i) + intercept2;
-
 end
 %%
 %++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
